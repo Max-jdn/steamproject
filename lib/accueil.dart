3d7d560ad26f92a16ce06f90ccc3fc7477626eac
simeongug <71648003+simeongug@users.noreply.github.com>
2023-03-29 15:57:09 +0200

    push

--- lib/accueil.dart
+++ lib/accueil.dart
@@ -6,6 +6,9 @@ import 'package:steamproject/wishlist.dart';
 import 'package:steamproject/wishlistvide.dart';
 import 'package:steamproject/meslikes.dart';
 import 'package:steamproject/meslikesvide.dart';
+import 'package:steamproject/api.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Accueil extends StatelessWidget {
   const Accueil({super.key});
@@ -23,6 +26,97 @@ class Accueil extends StatelessWidget {
             GameSection(),
           ],
         )));
+  }
+}
+
+Future<List<Rank>> fetchGames() async {
+  try {
+    final response = await http.get(
+      Uri.https(
+          'api.steampowered.com', '/ISteamChartsService/GetMostPlayedGames/v1'),
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials":
+            'true', // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Headers":
+            "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return await _getGameDetails(
+          Games.fromJson(jsonDecode(response.body)).ranks);
+    } else {
+      throw Exception('Failed to load games: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Failed to load games: $e');
+  }
+}
+
+Future<List<Rank>> _getGameDetails(List<Rank> ranks) async {
+  List<Rank> updatedRanks = [];
+  for (var rank in ranks) {
+    final response = await http.get(
+      Uri.parse(
+          'https://store.steampowered.com/api/appdetails?appids=${rank.appId}'),
+    );
+    if (response.statusCode == 200) {
+      final jsonBody = jsonDecode(response.body);
+      if (jsonBody[rank.appId.toString()]['success']) {
+        rank.name = jsonBody[rank.appId.toString()]['data']['name'];
+      }
+    }
+    updatedRanks.add(rank);
+  }
+  return updatedRanks;
+}
+
+class Games {
+  final int rollupDate;
+  final List<Rank> ranks;
+
+  Games({
+    required this.rollupDate,
+    required this.ranks,
+  });
+
+  factory Games.fromJson(Map<String, dynamic> json) {
+    return Games(
+      rollupDate: json['response']['rollup_date'],
+      ranks: List<Rank>.from(
+        json['response']['ranks'].map(
+          (rankJson) => Rank.fromJson(rankJson),
+        ),
+      ),
+    );
+  }
+}
+
+class Rank {
+  final int rank;
+  final int appId;
+  final int lastWeekRank;
+  final int peakInGame;
+  String? name;
+
+  Rank({
+    required this.rank,
+    required this.appId,
+    required this.lastWeekRank,
+    required this.peakInGame,
+    this.name,
+  });
+
+  factory Rank.fromJson(Map<String, dynamic> json) {
+    return Rank(
+      rank: json['rank'],
+      appId: json['appid'],
+      lastWeekRank: json['last_week_rank'],
+      peakInGame: json['peak_in_game'],
+      name: json['name'],
+    );
   }
 }
 
@@ -272,35 +366,29 @@ class Presentation extends StatelessWidget {
   }
 }
 
-class GameSection extends StatelessWidget {
+class GameSection extends StatefulWidget {
+  GameSection({super.key});
+  @override
+  State<GameSection> createState() => _GameSectionState();
+}
+
+class _GameSectionState extends State<GameSection> {
+  late Future<List<Rank>> futureGames;
+  @override
+  void initState() {
+    super.initState();
+    futureGames = fetchGames();
+  }
+
   final List gameList = [
     {
-      'nameJeu': 'Nom du Jeu',
-      'editor': 'Nom de l editeur',
-      'picture': 'assets/images/Destiny2pochette.png',
-      'price': '180',
-    },
-    {
-      'nameJeu': 'Nom du Jeu',
-      'editor': 'Nom de l editeur',
-      'picture': 'assets/images/Destiny2pochette.png',
-      'price': '180',
-    },
-    {
-      'nameJeu': 'Nom du Jeu',
-      'editor': 'Nom de l editeur',
-      'picture': 'assets/images/Destiny2pochette.png',
-      'price': '180',
-    },
-    {
-      'nameJeu': 'Nom du Jeu',
+      'nameJeu': 'nom du game',
       'editor': 'Nom de l editeur',
       'picture': 'assets/images/Destiny2pochette.png',
       'price': '180',
     },
   ];
 
-  GameSection({super.key});
   @override
   Widget build(BuildContext context) {
     return Container(
